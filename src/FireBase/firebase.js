1a222d29970c05efe4a452b4e4077409ad2a4d04
AndreFilipeGomes <45604807+AndreFilipeGomes@users.noreply.github.com>
2020-10-18 01:10:15 +0100

    Update firebase.js

--- src/FireBase/firebase.js
+++ src/FireBase/firebase.js
@@ -2,16 +2,7 @@
   import firebase from 'firebase';
 
   const firebaseApp = firebase.initializeApp({
-    //For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    apiKey: "AIzaSyB3YwxiMH-_6LfXXwwS8y0dJaO7rzDhVAQ",
-    authDomain: "todo-app-45593.firebaseapp.com",
-    databaseURL: "https://todo-app-45593.firebaseio.com",
-    projectId: "todo-app-45593",
-    storageBucket: "todo-app-45593.appspot.com",
-    messagingSenderId: "743664362916",
-    appId: "1:743664362916:web:acc8adb964f8348915cd31",
-    measurementId: "G-YD5M9NVV60"
-
+   //You need to paste here the configurations that firebase gives you when you create the DataBase 
   });
 
   const db = firebase.firestore();
